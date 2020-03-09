3c76f4c26e6739ce4bf7fb53721d85f8a9065144
AutoRunCD_Johnny <autoruncd.johnny@gmail.com>
2020-03-09 10:57:42 -0400

    some updates

--- bios_flasher.ino
+++ bios_flasher.ino
@@ -1,25 +1,31 @@
 #include<SPI.h>
 #include "rom.h"
 
+#define CE 99
+#define ROM_SIZE 2097152‬
+
 SPISettings bios(8000000,MSBFIRST,SPI_MODE3);
 byte state=0;
+byte readbuf[1024]={0};
+int i;
 
 void BIOS_WE(){
-  //CE low
+  digitalWrite(CE,LOW);
   SPI.transfer(0x06);
-  //CE high
+  digitalWrite(CE,HIGH);
 }
 
 void BIOS_RD(int address,int len,byte *buf){
   int i;
   byte addr[3]={address>>16,(address>>8)&0xFF,address&0xFF}; 
-  //CE low
+  
+  digitalWrite(CE,LOW);
   SPI.transfer(0x03);
   SPI.transfer(addr,3);
   for(i=0;i<len;i++){
     buf[i]=SPI.transfer(0x0);
   }
-  //CE high
+  digitalWrite(CE,HIGH);
 }
 
 //256byte each page
@@ -28,14 +34,14 @@ void BIOS_WR(int address,int page_no,const byte *buf){
   byte addr[3]={address>>16,(address>>8)&0xFF,address&0xFF},*p; 
   
   for(i=0;i<page_no;i++){
-    //CE low
+    digitalWrite(CE,LOW);
     SPI.transfer(0x02);
     SPI.transfer(addr,3);
     p=(byte*)(buf)+256*i;
     for(j=0;j<256;j++){
       SPI.transfer(p[j]);
     }
-    //CE high
+    digitalWrite(CE,HIGH);
   }
 
 }
@@ -45,18 +51,20 @@ void BIOS_CLR(int address,int blk_no){
   int i,j;
   byte addr[3]={address>>16,(address>>8)&0xFF,address&0xFF}; 
   for(i=0;i<blk_no;i++){
-    //CE low
+    digitalWrite(CE,LOW);
     SPI.transfer(0xD8);
     SPI.transfer(addr,3);
-    //CE high
+    digitalWrite(CE,HIGH);
   }
 }
 //ROM_size in byte
 void BIOS_flash(int ROM_size,int begin_addr,const byte *buf){
   int blk_no=ROM_size>>16,page_no=ROM_size>>8;
+  Serial.println("start flashing...\n");
   BIOS_CLR(begin_addr,blk_no);
   BIOS_WE();
   BIOS_WR(begin_addr,page_no,buf);
+  Serial.println("done...\n");
   //verification
   //...
 }
@@ -66,18 +74,24 @@ void setup() {
   Serial.begin(921600);
   SPI.begin();
   SPI.beginTransaction(bios);
+  pinMode(CE,OUTPUT);
+  digitalWrite(CE,HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println(analogRead(A0));
+  Serial.println("0 for flashing,1 for read binary data from bios,");
+  state=Serial.read();
   switch (state){
     case 0:
       BIOS_flash(2048*1024,0x200000,ROM);
-      state++;
       break;
     case 1:
-      Serial.println("ssgsgsg");
+      for(i=0;i<2048;i++){
+        BIOS_RD(i*1024,1024,readbuf);
+        Serial.println((char*)readbuf);
+      }
       break;
   }
   delay(1000);
